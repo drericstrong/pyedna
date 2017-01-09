b5b505165d5e0d6d56b7998d0a064a8358862e00
drericstrong <dr.eric.strong@gmail.com>
2017-01-08 21:49:28 -0500

    Update init file for version 0.12

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
   name='pyedna',
   packages=['pyedna'],
-  version='0.11',
+  version='0.12',
   description='Python wrapper library for the C++ EzDnaApi',
   author='Eric Strong',
   author_email='dr.eric.strong@gmail.com',
   url='https://github.com/drericstrong/pyedna',
-  download_url='https://github.com/drericstrong/pyedna/tarball/0.11',
+  download_url='https://github.com/drericstrong/pyedna/tarball/0.12',
   keywords=['eDNA', 'data'],
   classifiers=[],
 )
