d24a023d19af131c889c9f7b3d42e078eb13a791
drericstrong <dr.eric.strong@gmail.com>
2017-02-28 21:05:45 -0500

    Updating distributions
    Updating distributions

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
   name='pyedna',
   packages=['pyedna'],
-  version='0.17',
+  version='0.171',
   description='Python wrapper library for the C++ EzDnaApi',
   author='Eric Strong',
   author_email='dr.eric.strong@gmail.com',
   url='https://github.com/drericstrong/pyedna',
-  download_url='https://github.com/drericstrong/pyedna/tarball/0.17',
+  download_url='https://github.com/drericstrong/pyedna/tarball/0.171',
   keywords=['eDNA', 'data'],
   classifiers=[],
 )
